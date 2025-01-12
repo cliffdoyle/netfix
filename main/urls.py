e34781ef23c13903aac9ed29901f1b955d3b8f09
clomollo <oyoocliff471@gmail.com>
2025-01-12 21:24:18 +0300

    Added category filter to homepage for services; updated home view and template

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,7 @@ app_name = "main"
 urlpatterns = [
     path('', v.home, name='home'),
     path('logout/', v.logout, name='logout'),
-    # path('most_requested/', v.most_requested_services, name='most_requested_services'),
-    path('services/<str:field>/', v.services_by_category, name='services_by_category'),
+#    path('services/', v.services_by_category, name='services_by_category'),  # Handle empty field case
+    # path('services/<str:field>/', v.services_by_category, name='services_by_category_field'),  # Handle specific field case
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,42 +1,30 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth import logout as django_logout
 from services.models import Service
 from django.db.models import Count
 
 def home(request):
-      # Extract all predefined fields from the Service model's choices
+    # Extract all predefined fields from the Service model's choices
     fields = [choice[0] for choice in Service.FIELD_CHOICES]
 
     # Get the selected category from the GET request
     selected_field = request.GET.get('field', None)
 
-  # Filter services based on the selected category
+    # Filter services based on the selected category
     services = (
         Service.objects.filter(field=selected_field).order_by('-date_created')
         if selected_field
-        else None
+        else Service.objects.all()  # Default to all services if no field is selected
     )
-    
-    #Get the most requested services
+
+    # Get the most requested services
     most_requested_services = Service.objects.annotate(request_count=Count('servicerequest')).filter(request_count__gt=0).order_by('-request_count')[:10]
 
     return render(request, 'main/home.html', {
         'fields': fields,
         'services': services,
         'selected_field': selected_field,
-        'most_requested_services': most_requested_services,
-    })
-    
-def services_by_category(request, field):
-    fields = [choice[0] for choice in Service.FIELD_CHOICES]
-
-    # Filter services based on the selected category
-    services = Service.objects.filter(field=field).order_by('-date_created')
-
-    return render(request, 'main/services_by_category.html', {
-        'fields': fields,
-        'services': services,
-        'selected_field': field,
+        'most_requested_services': most_requested_services,  # Pass most requested services to the template
     })
 
 def logout(request):

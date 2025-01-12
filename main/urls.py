026b077a6d3075df933bea176e7692f918119f98
clomollo <oyoocliff471@gmail.com>
2025-01-12 18:34:14 +0300

    Fix: Rename template to `services_by_category.html` to resolve TemplateDoesNotExist error    - Ensures the template name matches the expected name in the views.

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,8 @@ app_name = "main"
 
 urlpatterns = [
     path('', v.home, name='home'),
-    path('logout/', v.logout, name='logout')
+    path('logout/', v.logout, name='logout'),
+    # path('most_requested/', v.most_requested_services, name='most_requested_services'),
+    path('services/<str:field>/', v.services_by_category, name='services_by_category'),
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import logout as django_logout
 from services.models import Service
-
+from django.db.models import Count
 
 def home(request):
       # Extract all predefined fields from the Service model's choices
@@ -16,12 +16,29 @@ def home(request):
         if selected_field
         else None
     )
+    
+    #Get the most requested services
+    most_requested_services = Service.objects.annotate(request_count=Count('servicerequest')).filter(request_count__gt=0).order_by('-request_count')[:10]
 
     return render(request, 'main/home.html', {
         'fields': fields,
         'services': services,
         'selected_field': selected_field,
+        'most_requested_services': most_requested_services,
     })
+    
+def services_by_category(request, field):
+    fields = [choice[0] for choice in Service.FIELD_CHOICES]
+
+    # Filter services based on the selected category
+    services = Service.objects.filter(field=field).order_by('-date_created')
+
+    return render(request, 'main/services_by_category.html', {
+        'fields': fields,
+        'services': services,
+        'selected_field': field,
+    })
+
 def logout(request):
     django_logout(request)
     return redirect("main:home")
